388f1205545748b393115da3a50f48aa903f9818
colinxfleming <c3flemin@gmail.com>
2017-12-11 00:32:47 -0500

    ever closer. bedtime though

--- lib/state_geo_tools/counters.rb
+++ lib/state_geo_tools/counters.rb
@@ -5,18 +5,20 @@ module StateGeoTools
     def count_instances(string, const)
       zone_counts = populate_counter_from const
 
-      puts zone_counts
       scratch_string = string
 
-      # Reverse is necessary here because some states are substrings
+      # Sort is necessary here because some states are substrings
       # of each other.
       # Looking at you, West Virginia / Virginia.
-      const.reverse.each do |zone|
+      zone_counts.keys.sort.each do |zone|
         puts zone
         puts scratch_string.scan(zone)
         zone_counts[zone] = scratch_string.scan(zone).count
-        scratch_string = scratch_string.delete zone
+        scratch_string.delete! zone
       end
+
+      puts zone_counts
+      puts scratch_string
 
       zone_counts.delete_if { |k, v| v.zero? }.keys
     end
