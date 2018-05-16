cf21ae5c4adfb609c9351523e87c64aa6938b8a5
colinxfleming <c3flemin@gmail.com>
2018-05-16 18:20:09 -0400

    appropriate freezing, do thecounting with a reducer

--- lib/state_geo_tools/counters.rb
+++ lib/state_geo_tools/counters.rb
@@ -1,34 +1,13 @@
 # frozen_string_literal: true
 
 module StateGeoTools
+  # Methods for counting instances in a string
   module Counters
     def count_instances(string, const)
-      zone_counts = populate_counter_from const
-
-      scratch_string = string
-
-      # Sort is necessary here because some states are substrings
-      # of each other.
-      # Looking at you, West Virginia / Virginia.
-      zone_counts.keys.sort.each do |zone|
-        puts zone
-        puts scratch_string.scan(zone)
-        zone_counts[zone] = scratch_string.scan(zone).count
-        scratch_string.delete! zone
+      instances = const.sort.reverse.map do |geo|
+        { geo => string.scan(/#{geo}/i).count }
       end
-
-      puts zone_counts
-      puts scratch_string
-
-      zone_counts.delete_if { |k, v| v.zero? }.keys
-    end
-
-    private
-
-    def populate_counter_from(const)
-      hasher = {}
-      const.each { |zone| hasher[zone] = 0 }
-      hasher
+      instances.reduce { |x, y| x.merge(y) }.delete_if { |_, v| v.zero? }
     end
   end
 end
--- lib/state_geo_tools/state_constants.rb
+++ lib/state_geo_tools/state_constants.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module StateGeoTools
   module States
     STATE_DATA = [
